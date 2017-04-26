40bfe29824b7728f88e2bdcea814dd8021333edd
thomasfrancisco <thofrancisco@gmail.com>
2017-04-26 12:25:06 +0200

    Ajout d'un shader permettant d'avoir un écran noir

---
+++ Assets/Script/CameraShaders.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShaders : MonoBehaviour {
+
+    private Material blackScreenShader;
+
+    private void Awake()
+    {
+        blackScreenShader = new Material(Shader.Find("Hidden/blackScreen"));
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, blackScreenShader);
+    }
+
+}
